e4e954c2399eb7d59399308f40282d460057682a
GitHub Action <action@github.com>
2022-12-13 03:38:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhangwei2811/testplugin/MyBundle.kt
+++ src/main/kotlin/com/github/zhangwei2811/testplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zhangwei2811.testplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zhangwei2811/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zhangwei2811/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhangwei2811.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhangwei2811.testplugin.MyBundle
 
 class MyApplicationService {
 
